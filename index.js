ad55071ae05757a1096054ee80c6c5a05514c5d8
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-01 17:21:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowLeft') {
+    moveDodgerLeft(e.key);
+  } else if (e.key === 'ArrowRight') {
+    moveDodgerRight(e.key);
+  }
+});
+
+function moveDodgerLeft(key) {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+function moveDodgerRight(key) {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
